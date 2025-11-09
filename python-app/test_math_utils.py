8f23f4c1adb5dd65954e341df30c44da02cda794
naoufelkhayati <naoufel@skillset.technology>
2025-11-09 03:22:44 +0100

    New structure 18

---
+++ python-app/test_math_utils.py
@@ -0,0 +1,9 @@
+import unittest
+from math_utils import factorial
+
+class TestFactorial(unittest.TestCase):
+    def test_factorial_5(self):
+        self.assertEqual(factorial(5), 120)
+
+if __name__ == '__main__':
+    unittest.main()
